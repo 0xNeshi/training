cf5a4746bf32349449dd6ec029309ca5ecf7666a
Nenad Misic <n.misic@outlook.com>
2021-12-19 09:57:30 +0100

    Modify block row alignment

--- src/components/BlockRow.js
+++ src/components/BlockRow.js
@@ -1,25 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
-function BlockRow({ repSchema, weights, amrap }) {
-  const [first, second, third] = weights;
-
-  return (
-    <Container>
-      <span>
-        <u>{repSchema}</u>
-        <i>{` - ${first}, ${second}, ${third}`}</i>
-      </span>
-      <span>+ {amrap}</span>
-    </Container>
-  );
-}
-
-export default BlockRow;
-
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
   font-size: 1.2em;
 `;
+
+const Text = styled.span`
+  flex-grow: 1;
+`;
+
+const RepSchema = styled(Text)`
+  text-decoration: underline;
+`;
+
+const Weights = styled.span`
+  flex-grow: 3;
+  font-style: italic;
+`;
+
+function BlockRow({ repSchema, weights, amrap }) {
+  const [first, second, third] = weights;
+
+  return (
+    <Container>
+      <RepSchema>{repSchema}</RepSchema>
+      <Text>-</Text>
+      <Weights>
+        {first} , {second} , {third}
+      </Weights>
+      <Text>x {amrap}</Text>
+    </Container>
+  );
+}
+
+export default BlockRow;
