796f5672a541f37dca9ac436f4c9a92c43a39d1c
Nenad Misic <n.misic@outlook.com>
2021-12-29 17:26:40 +0100

    Update Sign in page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container, CssBaseline } from "@mui/material";
+import { blue, common, grey, indigo, lime } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import Dashboard from "./components/Dashboard";
 import SignIn from "./components/SignIn";
-import UserProvider from "./providers/UserProvider";
 import { RequireAnon, RequireAuth } from "./guards";
+import UserProvider from "./providers/UserProvider";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 100vw;
-  background-color: #343028;
-  color: white;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   height: 100vh;
+//   width: 100vw;
+//   color: white;
+// `;
 
 const MainContainer = styled.div`
   height: 100vh;
 `;
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: indigo[600],
+    },
+    background: {
+      default: grey[900],
+      paper: grey[900],
+    },
+  },
+});
+
 function App() {
   return (
-    <UserProvider>
-      <Container>
-        <BrowserRouter>
-          <MainContainer>
-            <Routes>
-              <Route
-                exact
-                path="/signin"
-                element={
-                  <RequireAnon>
-                    <SignIn />
-                  </RequireAnon>
-                }
-              />
-              <Route
-                exact
-                path="/dashboard"
-                element={
-                  <RequireAuth>
-                    <Dashboard />
-                  </RequireAuth>
-                }
-              />
-            </Routes>
-          </MainContainer>
-        </BrowserRouter>
-      </Container>
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <Container component="main">
+          <CssBaseline />
+          <BrowserRouter>
+            <MainContainer>
+              <Routes>
+                <Route
+                  exact
+                  path="/signin"
+                  element={
+                    <RequireAnon>
+                      <SignIn />
+                    </RequireAnon>
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard"
+                  element={
+                    <RequireAuth>
+                      <Dashboard />
+                    </RequireAuth>
+                  }
+                />
+                <Route exact path="/" element={<Navigate to="/dashboard" />} />
+              </Routes>
+            </MainContainer>
+          </BrowserRouter>
+        </Container>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,8 @@
+import Button from "@mui/material/Button";
+import { blue } from "@mui/material/colors";
 import { useContext } from "react";
 import styled from "styled-components";
+import Google from "../assets/images/google.png";
 import { UserContext } from "../providers/UserProvider";
 import { signInWithGoogle } from "../services/authService";
 
@@ -17,12 +20,29 @@ const Error = styled.span`
   text-align: center;
 `;
 
+const Icon = styled.img`
+  padding: 10px;
+  width: 50px;
+  height: 100%;
+  margin-right: 15px;
+  background-color: white;
+  border-radius: 3px;
+`;
+
 const SignIn = () => {
   const { error } = useContext(UserContext);
 
   return (
     <Container>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <h3>Let's get started...</h3>
+      <Button
+        variant="contained"
+        onClick={signInWithGoogle}
+        sx={{ backgroundColor: blue[800], paddingLeft: 1 }}
+      >
+        <Icon src={Google} alt="Google logo" />
+        Connect with Google
+      </Button>
       {error && <Error>{error}</Error>}
     </Container>
   );
