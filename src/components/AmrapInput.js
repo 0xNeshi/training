e108bed216ebeeba8a8e26423417b43d1cc383e9
Nenad Misic <n.misic@outlook.com>
2022-02-02 18:22:14 +0100

    Make amrap reps empty if 0

--- src/components/AmrapInput.js
+++ src/components/AmrapInput.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const WAIT_INTERVAL = 1000;
 const ENTER_KEY = 13;
 
-export const AmrapInput = ({ reps = "", onChangeAmrapReps }) => {
+export const AmrapInput = ({ reps, onChangeAmrapReps }) => {
   const [amrapReps, setAmrapReps] = useState(reps);
 
   const handleChangeAmrapReps = useCallback(
@@ -31,7 +31,7 @@ export const AmrapInput = ({ reps = "", onChangeAmrapReps }) => {
 
   return (
     <Input
-      value={amrapReps}
+      value={amrapReps || ""}
       type="text"
       onChange={handleChange}
       onBlur={handleChangeAmrapReps}
