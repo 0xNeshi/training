8f86764b0ddd6258e1cc314707455274abc41fbc
Nenad Misic <n.misic@outlook.com>
2022-02-09 23:05:27 +0100

    Add webpack

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAdf-GlGOZMWTlSy5Dp1TIxUX5h2SY5Uxw",
+  authDomain: "training-711ed.firebaseapp.com",
+  projectId: "training-711ed",
+  storageBucket: "training-711ed.appspot.com",
+  messagingSenderId: "222988029466",
+  appId: "1:222988029466:web:0950feb53f6d5bed3b8ba3",
 };
 
 // Initialize Firebase
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PORT = 3000;
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  devServer: {
+    static: [path.join(__dirname, "dist"), path.join(__dirname, "public")],
+    port: PORT,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    fallback: {
+      buffer: require.resolve("buffer"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      manifest: "./public/manifest.json",
+      favicon: "./public/favicon.ico",
+      template: "./public/index.html",
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+};
