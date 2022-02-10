f95e0348d6cf928d46cbace48b8b27f4f27623f2
Nenad Misic <n.misic@outlook.com>
2022-02-10 06:56:12 +0100

    Remove redundant comments from guards and update manifest

--- src/guards.js
+++ src/guards.js
@@ -4,12 +4,10 @@ import { UserContext } from "./providers";
 
 export function RequireAuth({ children }) {
   const { user } = useContext(UserContext);
-  console.log("req auth");
   return !!user ? children : <Navigate to="/signin" />;
 }
 
 export function RequireAnon({ children }) {
   const { user } = useContext(UserContext);
-  console.log("req anon");
   return !user ? children : <Navigate to="/dashboard" />;
 }
